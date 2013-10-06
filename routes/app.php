b3d0c033b6475363d16255f6dcd02f2f94ff0e32
Misraim Mendoza <misraim.mendoza@mandragora-web-systems.com>
2013-10-06 15:12:34 -0500

    Elimino archivos que ya no se utilizan, y agrego la ruta para la vista de gasolineras

--- routes/app.php
+++ routes/app.php
@@ -6,3 +6,8 @@ $app->get('/', function() use ($app) {
 	$controller = new IndexController();
 	$app->render('app/index.html.twig', $controller->indexForm());
 })->name('home');
+
+$app->get('/gasolineras', function() use ($app) {
+	$controller = new IndexController();
+	$app->render('app/gas-station.html.twig', $controller->indexForm());
+})->name('gas-station');
